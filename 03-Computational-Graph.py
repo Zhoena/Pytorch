f47852d0c80085d2990631aeacef0b959106cd03
Zhoena <zhoenayanghao@163.com>
2020-03-23 16:24:32 +0800

    计算图

---
+++ 03-Computational-Graph.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+@brife      ：计算图示例
+"""
+
+import torch
+
+w = torch.tensor([1.], requires_grad=True)  # 1.为浮点数
+x = torch.tensor([2.], requires_grad=True)
+
+a = torch.add(w, x)  # retain_grad()
+# 只保存叶子结点的梯度，想要保存非叶子节点，用retain_grad()
+a.retain_grad()
+b = torch.add(w, 1)
+y = torch.mul(a, b)
+
+y.backward()
+
+print(w.grad)
+print(x.grad)
+
+# 查看叶子结点
+print("is_leaf:\n", w.is_leaf, x.is_leaf, a.is_leaf, b.is_leaf, y.is_leaf)
+
+# 查看梯度
+print("gradient:\n", w.grad, x.grad, a.grad, b.grad, y.grad)
+
+# 查看 grad_fn
+print("grad_fn:\n", w.grad_fn, x.grad_fn, a.grad_fn, b.grad_fn, y.grad_fn)
