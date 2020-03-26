63587b1db68d162155164c9fe9a6d8362dc3c4f7
Zhoena <zhoenayanghao@163.com>
2020-03-26 15:10:33 +0800

    修改LeNet 全连接层书写bug

--- model/lenet.py
+++ model/lenet.py
@@ -13,22 +13,24 @@ class LeNet(nn.Module):
     def __init__(self, classes):
         # 在__init__中定义网络需要的操作算子，比如卷积、全连接算子等等
         super(LeNet, self).__init__()
-        # Conv2d的第一个参数是输入的channel数量，第二个是输出的channel数量，第三个是kernel size
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        # nn.Conv2d的第一个参数是输入的channel数量，第二个是输出的channel数量，第三个是kernel size
+        # nn.Linear的第一个参数是输入的维数，第二个参数是输出的维数
+        # 输入 32 * 32 * 3
+        self.conv1 = nn.Conv2d(3, 6, 5)  # 得到(32-5+1)*(32-5+1))*6 = 28*28*6
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, classes)
 
     def forward(self, x):
-        out = F.relu(self.conv1(x))
-        out = F.max_pool2d(out, 2)
-        out = F.relu(self.conv2(out))
-        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv1(x))  # 得到(32-5+1)*(32-5+1))*6 = 28*28*6
+        out = F.max_pool2d(out, 2)  # 14*14*6
+        out = F.relu(self.conv2(out))  # (14-5+1)*(14-5+1)*16 = 10*10*16
+        out = F.max_pool2d(out, 2)  # 5*5*16 = 400
         out = out.view(out.size(0), -1)  # 将高维数据压缩成低维
-        out = F.relu(self.fc1(out))
-        out = F.relu(self.fc1(out))
-        out = self.fc3(out)
+        out = F.relu(self.fc1(out))  # 400 -> 120
+        out = F.relu(self.fc2(out))  # 120 -> 84
+        out = self.fc3(out)  # 84
         return out
 
     def initialize_weights(self):
